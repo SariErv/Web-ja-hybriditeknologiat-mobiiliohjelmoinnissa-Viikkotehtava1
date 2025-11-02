bace0e46c0bb26b8ff6f0b2c7cfab190155b680e
SariErv <t3ersa00students.oamk.fi>
2025-11-02 17:41:54 +0200

    Viikkotehtävä1 (tavoitesyke) palautus

--- App.tsx
+++ App.tsx
@@ -1,13 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState<string>("")
+
+  const isValid = age.trim() !== "" && !isNaN(Number(age))
+  const ageNum: number = isValid ? Number(age) : 0
+  
+  const lLimit = isValid ? ((220 - ageNum) * 0.65).toFixed(2) : "0"
+  const hLimit = isValid ? ((220 - ageNum) * 0.85).toFixed(2) : "0"
+  //Kentässä on 0 kun arvoa ei ole syötetty, näyttää tuloksen kahden desimaalin tarkkuudella
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.heading}>Heart Rate Limits Calculator</Text>
+      <Text>Enter your age:</Text>
+      <TextInput 
+      placeholder='age'
+      keyboardType='number-pad'
+      style={styles.input}
+      value={age}
+      onChangeText={setAge}
+    />
+    <Text style={styles.field}>Lower limit: {lLimit} bpm</Text>
+    <Text style={styles.field}>Higher limit: {hLimit} bpm</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +37,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 22
+  },
+  field: {
+    marginVertical: 2
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#00000042",
+    borderRadius: 6,
+    marginBottom: 16,
+    marginTop: 16,
+    width: 80
+  }
+})
